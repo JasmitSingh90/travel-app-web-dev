c65455c78f406e29f2eba28243e77e5dbbbf0b53
JasmitSingh90 <jasmitsingh90@gmail.com>
2025-05-19 14:59:59 +0530

    Updated site
    Updated site

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "8f539c47649c4f9270ef7d14e8f4eaf6",
+const RESOURCES = {"flutter_bootstrap.js": "110a9791c171800aba9bee342280295d",
 "version.json": "fe8eafefa9254f47aa651451ecf14651",
 "index.html": "8999e9885ac815ac95e744997924a933",
 "/": "8999e9885ac815ac95e744997924a933",
-"main.dart.js": "9115a1dfe14bdb642f5fc014964388f3",
+"main.dart.js": "80c52f836278f081eee1a06984c0a462",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "favicon.png": "946cded120674fc2dc184174af84a7e9",
 "icons/Icon-192.png": "63343ada61c2f41d27f13a01e4eb01ab",
