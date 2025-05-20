e7142bf5c26bb631056ff0f5195715736cdfe032
MHamouda <mhamouda@valu.com.eg>
2025-05-20 23:40:34 +0300

    updated

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "df266c70daa44f7dd62b26ad9d02e97c",
+const RESOURCES = {"flutter_bootstrap.js": "9c67038ae2420d6b9a5333d3329ad59a",
 "version.json": "fe8eafefa9254f47aa651451ecf14651",
 "index.html": "8999e9885ac815ac95e744997924a933",
 "/": "8999e9885ac815ac95e744997924a933",
-"main.dart.js": "f78fa2855e50c5c9a261f385cf998cfd",
+"main.dart.js": "984a73cb2e66e5167899b2e6e83135c2",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "946cded120674fc2dc184174af84a7e9",
 "icons/Icon-192.png": "63343ada61c2f41d27f13a01e4eb01ab",
@@ -36,7 +36,7 @@ const RESOURCES = {"flutter_bootstrap.js": "df266c70daa44f7dd62b26ad9d02e97c",
 "assets/packages/vad/assets/ort-wasm-simd.wasm": "49460a4b3ff6631a5475ff4e53ac5404",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/AssetManifest.bin": "e32dea7a42fc7ced42390735369cbf8d",
-"assets/fonts/MaterialIcons-Regular.otf": "eef6d64c96120dd09d4319bef0bb8a1b",
+"assets/fonts/MaterialIcons-Regular.otf": "0b7047096d107f7750da25240c632ebd",
 "assets/assets/images/chat_camera_image.svg": "13537106679ddfb3f23d43589ab93b53",
 "assets/assets/images/room_bg.png": "3ac523b7d4121144f6988754fc60d496",
 "assets/assets/images/transport_loading_2.png": "ab9a058499b0940303f5c8c803873daa",
