0e41a84bbe233b731872bc8ad92c073643582873
MHamouda <mhamouda@valu.com.eg>
2025-05-21 00:08:44 +0300

    updated version

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "068785fb396ab9fa19900c7f2ecb281c",
+const RESOURCES = {"flutter_bootstrap.js": "1858aae1e26cf9b338ca84595c729b74",
 "version.json": "fe8eafefa9254f47aa651451ecf14651",
 "index.html": "8999e9885ac815ac95e744997924a933",
 "/": "8999e9885ac815ac95e744997924a933",
-"main.dart.js": "2bda99bf5601619a83d5f6fb5fcedf3c",
+"main.dart.js": "5e0de5fe33740a49090eaae7dc544001",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "946cded120674fc2dc184174af84a7e9",
 "icons/Icon-192.png": "63343ada61c2f41d27f13a01e4eb01ab",
