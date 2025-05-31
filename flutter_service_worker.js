33eba92a00b743da8ed701104b3078507787cbcf
JasmitSingh90 <jasmitsingh90@gmail.com>
2025-06-01 03:17:58 +0530

    Updated changes
    Updated changes

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "a7a47eb3f7843676415c83173cd49b6d",
+const RESOURCES = {"flutter_bootstrap.js": "b01116d4256d166ca4d8a62d1a2ce793",
 "version.json": "ece5d41d23aa15064a206cae4a818c7f",
 "index.html": "8999e9885ac815ac95e744997924a933",
 "/": "8999e9885ac815ac95e744997924a933",
-"main.dart.js": "c9d7dc1cfcbddeb1daf8284c98bf65a1",
+"main.dart.js": "f6e0508ef0ebf7cbf236dbcaf7fe3acf",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "favicon.png": "9ec3cdc0277dd02c19db0fcc625a2e6a",
 "icons/Icon-192.png": "f227577269e6a65b6b0f2c30f07f2f48",
